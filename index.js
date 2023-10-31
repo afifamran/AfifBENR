703d4ea79a29452b4919ea2e8a69fe099cb2a6e0
afifamran <b022210075@student.utem.edu.my>
2023-10-31 12:09:14 +0800

    Apip

--- index.js
+++ index.js
@@ -1,10 +1,36 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000;
+
 app.use(express.json())
+
 app.get('/', (req, res) => {
  res.send('Hello World!')
 })
+
+/*app.post('/login', (req, res) => {
+    console.log(req.body.username, req.body.password)
+    
+    //Check if username invalid
+    if(req.body.username != 'saya'){
+        return res.status(400).send('Invalid User')
+    }
+
+    if(req.body.password != '123'){
+        return res.status(400).send('Invalid Password')
+    }
+
+    res.send('login successfully')
+})*/
+
+//Efficient way
+app.post('/login', (req, res) => {
+    if(req.body.username != 'saya' || req.body.password != '123'){
+        return res.status(400).send('Invalid User or Password')
+    }
+    res.send('login successfully')
+})
+
 app.listen(port, () => {
  console.log(`Example app listening on port ${port}`)
 })
